36046a7a57ba87406921f5b37830fc8c2d1d8d39
himel2535 <monwarhossanhimel@gmail.com>
2025-09-23 14:44:13 +0600

    Set Layout and make react responsive

--- src/components/AvailablePlayers/AvailablePlayers.jsx
+++ src/components/AvailablePlayers/AvailablePlayers.jsx
@@ -1,14 +1,29 @@
-import React, { use } from 'react';
+import React, { use } from "react";
 
-const AvailablePlayers = ({playerPromise}) => {
-
-    const playerData=use(playerPromise)
-    console.log(playerData)
-    return (
-        <div>
-            <h2>available</h2>
+const AvailablePlayers = ({ playerPromise }) => {
+  const playerData = use(playerPromise);
+  console.log(playerData);
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-5">
+      {playerData.map((player) => (
+        <div className="card p-4 bg-base-100 w-96 shadow-sm">
+          <figure>
+            <img className=" max-h-[150px] object-cover" src={player.playerImg} alt="Shoes" />
+          </figure>
+          <div className="card-body">
+            <h2 className="text-lg font-bold">{player.playerName}</h2>
+            <h5 className="text-base">Rating: {player.playerRating}</h5>
+            <p className="text-base"></p>
+            <p>{player.playerRole}</p>
+            <p>price: {player.price}</p>
+            <div className="card-actions justify-end">
+              <button className="btn">Choose Player</button>
+            </div>
+          </div>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default AvailablePlayers;
